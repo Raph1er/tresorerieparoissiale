4bf4cffe500eb2dc0f48b73c521738c8c4ab1876
Raphaël <henriraphael46@gmail.com>
2026-04-01 12:51:51 +0200

    Dîmes format de libellé

--- modules/dimes/dime.service.ts
+++ modules/dimes/dime.service.ts
@@ -17,10 +17,10 @@ import type {
   UpdateRepartitionDimeDTO,
 } from './dime.types';
 
-const NOM_CATEGORIE_ENTREE = 'Dîmes';
-const NOM_CATEGORIE_PAROISSE_MERE = 'Dîmes - Paroisse Mère';
-const NOM_CATEGORIE_RESPONSABLE = 'Dîmes - Responsable';
-const NOM_CATEGORIE_LEVITES = 'Dîmes - Lévites';
+const NOM_CATEGORIE_ENTREE = 'Dîmes Décaissée';
+const NOM_CATEGORIE_PAROISSE_MERE = 'Vers la Paroisse Mère';
+const NOM_CATEGORIE_RESPONSABLE = 'Dîmes au Chargé';
+const NOM_CATEGORIE_LEVITES = 'Dîmes aux Lévites';
 
 function parseDateInput(value: Date | string): Date {
   if (value instanceof Date) {
